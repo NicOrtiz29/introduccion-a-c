145474e3f8402adbf0ffd5248ee99d6c98a33542
Nahuel Barrios <barrios.nahuel@gmail.com>
2019-04-18 19:45:39 -0300

    Respondo las dudas

--- main.c
+++ main.c
@@ -18,25 +18,28 @@
 */
 
 int main() {
-    float calificaciones[5];  //no entiendo porque le pone el 5.. si despues repercute en algo o no.
+    float calificaciones[5];  //no entiendo porque le pone el 5.. si despues repercute en algo o no. ==> RTA: Es el tamaño del array. Solo puede tener 5 elementos. A los array hay que definirlos con un tamaño si o si.
     float promedio = 0;  //aca esta diciendo que arranca en 0,..
 
     printf("programa que calucla promedio de 5 califiaciones..... \n");
 
-    for (int i = 0; i <
-                    5; i++) {    // aca declara la variable "i" como numero entero para que arranque en "0"... no entiendo lo que sigue.. que "i" tiene que ser menor que "5"?? despues a i e suma un numero????
-        printf("dame las calificaciones %d:     ",
-               i + 1); // Al ponerlo asi pido la calificacion 1, luego la 2 y asi hasta 5..
-        scanf("%f",
-              &calificaciones[i]); // guarda la variable numerica en calificaciones y no entiendo que es lo que hace la "i" entre corchetes.
+    for (int i = 0; i < 5; i++) {    // aca declara la variable "i" como numero entero para que arranque en "0"... no entiendo lo que sigue.. que "i" tiene que ser menor que "5"?? despues a i le suma un numero????
+        // RTA: Sí. El for tiene 3 partes. 1. La variable con la que vas a laburar; 2. Una condición (i<5), y mientras esto de true el for va a seguir repitiendose eternamente; 3. El incremento de la variable de control (o sea i) que es la que define cuando hay que salir del for.
+
+        printf("dame las calificaciones %d:", i + 1); // Al ponerlo asi pido la calificacion 1, luego la 2 y asi hasta 5..
+
+        scanf("%f", &calificaciones[i]); // guarda la variable numerica en calificaciones y no entiendo que es lo que hace la "i" entre corchetes.
+        // RTA: La entre corchetes es para acceder al elemento en la posición "i" del arrray.
+
         promedio += calificaciones[i]; // no entiendo porque pone el += ademas de la [i]
+        // RTA: += es lo mismo que decir:
+        // promedio = promedio + calificaciones[i] (es como un atajo)
+        // y por otro lado calificaciones[i] como te puse arriba es para acceder al valor que hay en la posición "i" del array "calificaciones".
+
         printf("\n");
-
-
     }
 
 
-    printf("el promedio de las calificaciones es:     %.2f\n\n",
-           promedio / 5);  //esto seria que el numero que le dio el promedio lo divda x5?
+    printf("el promedio de las calificaciones es: %.2f\n\n", promedio / 5);  // Esto seria que el numero que le dio el promedio lo divda x5? ==> RTA: Sí.
     return 0;
 }
