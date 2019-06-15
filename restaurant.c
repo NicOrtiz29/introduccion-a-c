cb099f914d069856397a4c53f3d492bb29401db9
nicolas <nicortiz29@gmail.com>
2019-06-15 17:39:02 -0300

    arranco con el ejercicio del restaurante

--- restaurant.c
+++ restaurant.c
@@ -5,6 +5,23 @@
 #include "stdio.h"
 #include "restaurant.h"
 
+#define CANTIDAD 4
+
 void runRestaurantExercise() {
-    printf("Running function runRestaurantExercise");
+    printf("Running function runRestaurantExercise\n");
+
+    char platos[CANTIDAD][50];
+    float precios[CANTIDAD];
+
+
+    for (int i = 0; i < CANTIDAD; i++) {
+        printf("ingrese el nombre del plato %d: \n", i + 1);
+        scanf("%s", platos[i]);
+
+    }
+
+    for (int i = 0; i < CANTIDAD; i++) {
+        printf("%s", platos[i]);
+    }
+
 }
